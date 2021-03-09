c5bec97ba7ee6ad328b2f558785b681006996b66
Pakschoudhary <38201027+Pakschoudhary@users.noreply.github.com>
2021-03-09 19:03:45 +0530

    Add files via upload

---
+++ functionAS[1].java
@@ -0,0 +1,19 @@
+public class functionAS {
+
+    public static void det(String a, String b, String c, String d)
+    {
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("The details of the students is: ");
+        det("Akshay", "Mumbai", "Adypu", "MCA-Data Science");
+
+    }
+}
