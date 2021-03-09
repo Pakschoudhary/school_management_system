cae92c19e83acb05b62059771741f9f835425e50
Pakschoudhary <38201027+Pakschoudhary@users.noreply.github.com>
2021-03-09 19:18:16 +0530

    Add files via upload

---
+++ swap[1].java
@@ -0,0 +1,11 @@
+public class swap {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int temp;
+        temp = x;
+        x = y;
+        y = temp;
+        System.out.println("Swapped value is " + x + " " + y);
+    }
+}
