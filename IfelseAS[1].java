0f8d24267c31df6f4719a67f687214c21d871827
Pakschoudhary <38201027+Pakschoudhary@users.noreply.github.com>
2021-03-09 19:06:22 +0530

    Add files via upload

---
+++ IfelseAS[1].java
@@ -0,0 +1,18 @@
+public class IfelseAS {
+    public static void main(String[] args) {
+        int a = 10;
+        if (a > 10) {
+            System.out.println(a);
+        } else {
+            System.out.println("Grater");
+        }
+
+        if (a > 10) {
+            System.out.println(a);
+        } else if (a == 10) {
+            System.out.println("equals");
+        } else {
+            System.out.println("not order");
+        }
+    }
+}
