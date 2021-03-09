51578117c7a2e742f8fb395250b0c845796c6f90
Pakschoudhary <38201027+Pakschoudhary@users.noreply.github.com>
2021-03-09 19:20:59 +0530

    Add files via upload

---
+++ variable[1].java
@@ -0,0 +1,51 @@
+
+public class variable { // declearing a class name variable
+    public static void main(String[] args) { // declearingf a main function
+
+        // In below we are declearing 2 variables and printing the values
+        
+        int x; // declearing a variable name x
+        x = 20; // giving variable x a value of 20
+        System.out.println(x); // printing a value of x as output
+        int X = 30; // declearing a variable name X and assigning a value to X as 30
+        System.out.println(X); // printing X
+
+        // printing strings ans well as concatinating both strings
+        
+        String s = "Akshay";  // Declearing a string s as Akshay
+        System.out.println(s); // printing a value of string s
+        String p = "hello"; // Declearing a string p as hello
+        String q = "Developers"; //Declearing a string q as developers
+        System.err.println(p + " " + q); // concatenating the srtings p and q
+
+        // printing both int and string together
+
+        String z = "hi"; // declearing a string z as hi
+        int y = 5; // declearing int y as 5
+        System.out.println(y + z); // adding both string and int 
+
+        // printing strings assigning int as a var
+
+        String m = "2"; // declearing a string m as 2
+        String n = "10"; // declearing a string n as 10
+        System.out.println(m + n); // adding both the strings
+
+        // printing statement
+
+        int r = 6; //declearing int r as 6
+        System.out.println("Number =" + r); // printing rwith pre written as number
+
+        // area of circle
+
+        int f = 3; // defining f as radius of circle as 3
+        Float pi = 3.14f; // declearing pi value by giving double
+        System.out.println("Area of circle =" + pi * f * f); // printing ar of circle 
+
+        // area of rectangle
+         
+        int l = 10; // defining int l as 10
+        int b = 20; // defining int b as 20
+        System.out.println("Area of rectangle is: " + l*b); // printing ar of rectangle l*b
+
+    }
+}
