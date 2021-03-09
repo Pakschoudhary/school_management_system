747c23d96118e78943b177cc7ddd65f013920717
Pakschoudhary <38201027+Pakschoudhary@users.noreply.github.com>
2021-03-09 19:10:32 +0530

    Add files via upload

---
+++ lengthstr[1].java
@@ -0,0 +1,12 @@
+public class lengthstr {
+    public static void main(String[] args) {
+        String a = "Akshay";
+        String b = "Kekunnaya";
+        System.out.println("Length of first string is " + a.length());
+        System.out.println("Length of second string is " + b.length());
+        System.out.println("Conctetation of a and b is " + (a.concat(b)));
+        System.out.println("Upper case is " + a.toUpperCase());
+        System.out.println("Lower case of b is " + b.toLowerCase());
+        
+    }
+}
